0f16837978a5a660e900f87f2d8779ce835f4efc
Irawancoy <101044546+Irawancoy@users.noreply.github.com>
2025-02-26 19:04:36 +0700

    user service initialization

---
+++ smmsb-user-service/src/main/java/com/microservices/smmsb_user_service/SmmsbUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.smmsb_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmsbUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmmsbUserServiceApplication.class, args);
+	}
+
+}
---
+++ smmsb-user-service/src/test/java/com/microservices/smmsb_user_service/SmmsbUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.smmsb_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmmsbUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
