6b16f9e09c3c0196cf2b96488433469018430b77
Irawancoy <101044546+Irawancoy@users.noreply.github.com>
2025-02-28 09:58:54 +0700

    api gateway initialization

---
+++ smmsb-api-gateway/src/main/java/com/microservices/smmsb_api_gateway/SmmsbApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.smmsb_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmsbApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmmsbApiGatewayApplication.class, args);
+	}
+
+}
